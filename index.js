8c3f4b8334ae07f400618a5c8326aaad80a3c399
fortunesbusiness <79787059+fortunesbusiness@users.noreply.github.com>
2021-03-04 09:39:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ mongoose.connect(DB_URI,()=>{
 //cloudinary configuration
 //cloudinary configuration
 cloudinary.config({ 
-    cloud_name: 'dzywuv120', 
-    api_key: '383977439748697', 
-    api_secret: 'N_66lZPmjQqdEkx0Rcs5iKHnimg' 
+    cloud_name: 'dbefofzqn', 
+    api_key: '685581139743957', 
+    api_secret: 'KZMOdAMwbuNw6pOJHrDq_nsig4Y' 
 });
 
 //initialize helper libraries
