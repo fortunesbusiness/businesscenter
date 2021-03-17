dbd56a75c2744493937939b69009e0308bc50b91
fortunesbusiness <79787059+fortunesbusiness@users.noreply.github.com>
2021-03-17 16:37:08 +0600

    Update admin.route.js

--- Admin/admin.route.js
+++ Admin/admin.route.js
@@ -6,6 +6,8 @@ const {verifyAdmin} = require('../middleware/verify.token');
 
 //route for admin
 router.post('/create',adminController.createAdmin);
+router.post('/change/username',adminController.updateAdminUsername);
+router.post('/change/password',adminController.updateAdminPassword);
 router.post('/login',adminController.adminLogin);
 router.get('/dashboard',verifyAdmin,adminController.getTotalRequestAndRegisteredmember);
 
