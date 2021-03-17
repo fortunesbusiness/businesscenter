da68c9fb289feacdcc8f65dc08c976615e14e25c
fortunesbusiness <79787059+fortunesbusiness@users.noreply.github.com>
2021-03-17 16:38:55 +0600

    Update business.center.route.js

--- BusinessCenter/business.center.route.js
+++ BusinessCenter/business.center.route.js
@@ -7,5 +7,7 @@ const businessCenterController = require('./business.center.controller');
 //route for business center routes
 router.get('/information',verifyAdmin,businessCenterController.businessCenterShareAndDepositInformation);
 router.post('/add/expense',verifyAdmin,businessCenterController.addExpense);
+router.post('/change/deposit',verifyAdmin,businessCenterController.changeDepositAmount);
+router.post('/change/expense',verifyAdmin,businessCenterController.changeExpense);
 
 module.exports = router;
