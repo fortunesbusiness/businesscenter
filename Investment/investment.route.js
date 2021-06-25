03843d1407b157d3d1ee2f102de16117eff99e06
fortunesbusiness <79787059+fortunesbusiness@users.noreply.github.com>
2021-06-25 21:57:02 +0600

    Update investment.route.js

--- Investment/investment.route.js
+++ Investment/investment.route.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const {verifyUser,verifyAdmin} = require('../middleware/verify.token');
+const {verifyAdmin} = require('../middleware/verify.token');
 const investmentController = require('./investment.controller');
 
 //route for user routes
 router.get('/list',verifyAdmin,investmentController.getInvestmentRecord);
+router.post('/update/total',verifyAdmin,investmentController.updateTotalInvestmentAmount);
+router.post('/update/food',verifyAdmin,investmentController.updateFoodInvestmentAmount);
+router.post('/update/health',verifyAdmin,investmentController.updateHealthInvestmentAmount);
+router.post('/update/vehicle',verifyAdmin,investmentController.updateVehicleInvestmentAmount);
+router.post('/update/education',verifyAdmin,investmentController.updateEducationInvestmentAmount);
+router.post('/update/residence',verifyAdmin,investmentController.updateResidenceInvestmentAmount);
+router.post('/update/garments',verifyAdmin,investmentController.updateClothsInvestmentAmount);
 module.exports = router;
